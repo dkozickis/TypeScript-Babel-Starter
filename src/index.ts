2590cea33e92061f5207999f73f34f3bca65d3c9
Deniss Kozickis <deniss.kozickis@gmail.com>
2018-08-31 00:27:20 +0300

    Init - Second

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-export class C {
-    private x = 10;
-    getX = () => this.x;
-    setX = (newVal: number) => {
-        this.x = newVal;
-    }
-}
-
-export let x = new C();
-export let y = {...{some: "value"}};
-
 interface TestInterface<S, P> {
     [key: string]: (payload: P) => S;
 }

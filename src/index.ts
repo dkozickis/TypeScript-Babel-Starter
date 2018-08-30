9eceec09e8d689ad819b03c33854ab52d5e29a26
Deniss Kozickis <deniss.kozickis@gmail.com>
2018-08-31 00:17:45 +0300

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 export class C {
-    private x = 10
+    private x = 10;
     getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
+    setX = (newVal: number) => {
+        this.x = newVal;
+    }
 }
 
 export let x = new C();
-export let y = { ...{ some: "value" } }
+export let y = {...{some: "value"}};
+
+interface TestInterface<S, P> {
+    [key: string]: (payload: P) => S;
+}
+
+type TestReturner<S, P> = () => TestInterface<S, P>;
+
+const testConst: TestReturner<string, {batch: string; year: number}> = () => ({
+    'aaaaa': (payload) => {
+        return payload.batch;
+    },
+});

39e1bf3196354c105a45684063cadce2cad7592a
Deniss Kozickis <deniss.kozickis@gmail.com>
2018-08-31 00:25:48 +0300

    Init - Second

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,14 @@ interface TestInterface<S, P> {
 
 type TestReturner<S, P> = () => TestInterface<S, P>;
 
+const TEST_TYPE = 'aaaa';
+const TEST_TYPE_SEC = 'OOO';
+
 const testConst: TestReturner<string, {batch: string; year: number}> = () => ({
-    'aaaaa': (payload) => {
+    [TEST_TYPE](payload) {
         return payload.batch;
+    },
+    [TEST_TYPE_SEC](payload) {
+        return `${payload.year}`;
     },
 });
